3327b56a0e7a35678b3b5addc53338e6a42a8ddc
yanbo.zhou <chenggou124@gmail.com>
2025-07-24 00:35:23 +0800

    添加新的插件以及映射

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,3 +2,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Tree" })
 
+
+
+vim.keymap.set("n", "<leader>q", "<cmd>close<CR>", { desc = "关闭当前窗口" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "强制退出所有窗口" })
+vim.keymap.set("n", "<leader>o", "<cmd>only<CR>", { desc = "只保留当前窗口" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "关闭当前 buffer" })
+
+
--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -25,12 +25,66 @@ return {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+        -- Rainbow parentheses
+        rainbow = {
+          enable = true,
+          extended_mode = true,
+          max_file_lines = nil,
+        }
       },
       indent = {
         enable = true,
       },
     })
   end,
+},
+{
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      size = 15,
+      open_mapping = [[<C-\>]], -- 默认 Ctrl+\ 切换开关
+      hide_numbers = true,
+      shade_terminals = true,
+      start_in_insert = true,
+      persist_size = true,
+      direction = "horizontal", -- 也可以是 vertical、float、tab
+      close_on_exit = true,
+      shell = vim.o.shell,
+    })
+
+    -- 自定义终端编号
+    local Terminal = require("toggleterm.terminal").Terminal
+
+    -- 终端 1
+    local term1 = Terminal:new({ count = 1, direction = "horizontal" })
+    vim.keymap.set("n", "<leader>1", function() term1:toggle() end, { desc = "切换终端 1" })
+
+    -- 终端 2
+    local term2 = Terminal:new({ count = 2, direction = "horizontal" })
+    vim.keymap.set("n", "<leader>2", function() term2:toggle() end, { desc = "切换终端 2" })
+
+    -- 浮动终端（独立窗口）
+    local float_term = Terminal:new({ direction = "float" })
+    vim.keymap.set("n", "<leader>tf", function() float_term:toggle() end, { desc = "浮动终端" })
+
+
+            -- terminal 模式下退出 insert（进入 normal）
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+
+-- normal 模式下重新进入 terminal insert
+vim.keymap.set("t", "i", "i", { noremap = true })
+
+-- normal 模式下使用 :q 关闭
+    vim.keymap.set("t", "<leader>q", "<C-\\><C-n>:q<CR>", { noremap = true })
+
+            vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]])
+vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]])
+
+  end,
 }
+
+
 
 }
