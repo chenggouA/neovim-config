dbd3a4e88cc63e6ce2d426315afd74823edff5ee
yanbo.zhou <yanbo.zhou@CL-PC0121>
2025-07-30 17:14:36 +0800

    添加在当前文件中查找的快捷键并美化界面

--- lua/core/keymaps/telescope.lua
+++ lua/core/keymaps/telescope.lua
@@ -3,6 +3,7 @@ local M = {}
 M.keys = {
 	{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "查找文件" },
 	{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "全局搜索" },
+	{ "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "当前文件搜索" },
 }
 
 return M
--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -3,5 +3,39 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = require("core.keymaps.telescope").keys,
 	cmd = "Telescope",
-	opts = true,
+	opts = function()
+		return {
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = {
+					height = 0.9,
+					width = 0.9,
+					prompt_position = "top",
+					mirror = true,
+				},
+				sorting_strategy = "ascending",
+				prompt_prefix = " 🔍 ",
+				selection_caret = "❯ ",
+				border = true,
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				mappings = {
+					i = {
+						["<ESC>"] = require("telescope.actions").close,
+						["<C-j>"] = require("telescope.actions").move_selection_next,
+						["<C-k>"] = require("telescope.actions").move_selection_previous,
+					},
+				},
+			},
+			pickers = {
+				current_buffer_fuzzy_find = {
+					previewer = false,
+					sorting_strategy = "ascending",
+					layout_strategy = "vertical",
+					layout_config = {
+						prompt_position = "top",
+					},
+				},
+			},
+		}
+	end,
 }
