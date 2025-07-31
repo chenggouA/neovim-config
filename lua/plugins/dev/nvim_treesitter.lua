2a71f3c5a10a41b528e125497080a4db2d3efe34
chenggou <chenggou124@gmail.com>
2025-07-31 15:02:40 +0800

    feat:根据系统动态调整treesitter的编译器

--- lua/plugins/dev/nvim_treesitter.lua
+++ lua/plugins/dev/nvim_treesitter.lua
@@ -6,8 +6,28 @@ return {
 		"HiPhish/rainbow-delimiters.nvim",
 	},
 	config = function()
+        local os_name = vim.loop.os_uname().sysname
+
+        -- 根据操作系统设置合适的编译器
+        local compilers
+        if os_name == "Linux" then
+            -- Linux系统使用gcc
+            compilers = { "gcc" }
+        elseif os_name == "Darwin" then
+            -- macOS系统使用clang
+            compilers = { "clang" }
+        else
+            -- 其他系统默认使用gcc
+            compilers = { "gcc" }
+        end 
+
+        -- 应用编译器配置到nvim-treesitter
+        require("nvim-treesitter.install").compilers = compilers
+
+        -- 可选：打印当前使用的编译器信息，方便调试
+        vim.notify(string.format("nvim-treesitter 将使用以下编译器: %s", table.concat(compilers, ", ")))
+
 		require("nvim-treesitter.install").prefer_git = true
-		require("nvim-treesitter.install").compilers = { "clang" }
 
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
