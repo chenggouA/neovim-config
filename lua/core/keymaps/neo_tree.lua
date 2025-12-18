e8766c85e8a45993c01f77e6ab87aefb4af05e5a
chenggou <chenggou124@gmail.com>
2025-12-18 16:02:52 +0800

    refactor: 简化 Neo-tree 按键映射
    - 移除冗余的 neo_tree_utils.lua 工具模块
    - 在 keymaps 中直接使用命令字符串替代函数包装
    - 保持功能不变，减少代码层级

--- lua/core/keymaps/neo_tree.lua
+++ lua/core/keymaps/neo_tree.lua
@@ -1,9 +1,7 @@
 local M = {}
 
-local neo_utils = require("plugins.navigation.neo_tree_utils")
-
 M.keys = {
-    { "<leader>e", neo_utils.focus_neotree, desc = "文件树" },
+	{ "<leader>e", "<cmd>Neotree focus<cr>", desc = "文件树" },
 }
 
 return M
--- lua/plugins/navigation/neo_tree_utils.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
--- 聚焦到 Neo-tree 窗口（如果未打开则打开）
-function M.focus_neotree()
-	vim.cmd("Neotree focus")
-end
-
-return M
